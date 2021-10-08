7ae82587bbfd35e9e77da36ce8c9e2cef990c1df
AdamBucko1 <xbuckoa@stuba.sk>
2021-10-08 17:11:26 +0200

    Addapting scales done

--- src/main/java/sk/stuba/fei/uim/oop/DrawAxis.java
+++ src/main/java/sk/stuba/fei/uim/oop/DrawAxis.java
@@ -22,12 +22,14 @@ public class DrawAxis {
         this.graphOffSet= graphOffSet;
         drawAxisScale();
     }
+    //toto je základná podoba mierky ktorá sa neskôr upravý na požadovanú veľkosť, je volaná z metódy drawAxisScale
     private void fillListOfScale(double[] listOfYScale){
         for (int i=0;i<listOfYScale.length;){
             listOfYScale[i]=0.001*++i;
         }
 
     }
+    //Vynásobíme mierku premenou scaleIncreaser (ktorá sa vypočíta v metóde drawAxisScale) aby naša mierka korešpondovala s vykresleným grafom
     private void setAxisUpToScale(double[] listOfYScale, long scaleIncreaser){
         for (int i=0;i<listOfYScale.length;i++){
             listOfYScale[i]=listOfYScale[i]*scaleIncreaser;
@@ -36,20 +38,24 @@ public class DrawAxis {
     }
 
     public void drawAxisScale(){
+        //vykreslenie "holej" y osi
         if (isY){
             graph.setPaint(Color.BLACK);
         graph.draw(new Line2D.Double(graphOffSet, height/2,width-graphOffSet, height/2));} //X axis
 
+        //vykreslenie "holej" x osi
         else {
             graph.setPaint(Color.BLACK);
         graph.draw(new Line2D.Double(width/2, graphOffSet, width/2, height-graphOffSet));} //Y axis
 
+        //ideme vypočítať scaleIncreaser
         long scaleIncreaser=10;
         double[] listOfScale =new double[10];
         fillListOfScale(listOfScale);
 
         while (true){
             if (endOfAxis < 0.01*scaleIncreaser) {
+                //keď sme vypočítali scaleIncreaser môžme mierku nastaviť do požadovanej veľkosti
                 setAxisUpToScale(listOfScale,scaleIncreaser);
                 break;
 
@@ -62,10 +68,14 @@ public class DrawAxis {
                 scaleIncreaser=scaleIncreaser+scaleIncreaser;}
 
         }
+        //osadíme 0 do grafu
         graph.setPaint(Color.BLACK);
-        graph.drawString("0",(int)width/2+3,(int)height/2+15 );
+        graph.drawString("0",(int)width/2+3,(int)height/2+15
+
+        );
         for (int i = 0; i< listOfScale.length; i++){
             if (listOfScale[i]> endOfAxis){
+                ////Algoritmus pre grafické osadenie koncovej hodnoty na osi Y +9 medzičiar.
                 if (isY==true){
                     graph.draw(new Line2D.Double(width/2-2, height/2- endOfAxis * axisScale,width/2+2, height/2- endOfAxis * axisScale));
                     graph.drawString(Double.toString(endOfAxis), (int) (width / 2 + 2), Math.round((height / 2 - (endOfAxis* axisScale))));
@@ -79,6 +89,8 @@ public class DrawAxis {
 
                     }
                 }
+
+                ////Algoritmus pre grafické osadenie koncovej hodnoty na osi X+ 9 medzičiar.
                 else {
                     graph.draw(new Line2D.Double(width/2- endOfAxis * axisScale, height/2-2,width/2- endOfAxis * axisScale, height/2+2));
                     graph.drawString(Double.toString(endOfAxis), Math.round((width / 2 + (endOfAxis* axisScale))-3),(int) height/2+15);
@@ -93,7 +105,9 @@ public class DrawAxis {
                 }
                 break;
             }
+
             else {
+                //Algoritmus pre grafické osadenie mierky na os Y aj s medzičiarami medzi číselne vyjadrenými bodmi mierky.
                 if (isY==true) {
                     graph.draw(new Line2D.Double(width / 2 - 2, height / 2 - listOfScale[i] * axisScale, width / 2 + 2, height / 2 - listOfScale[i] * axisScale));
                     graph.drawString(Double.toString(listOfScale[i]), (int) (width / 2 + 2), Math.round((height / 2 - (listOfScale[i]* axisScale))));
@@ -105,6 +119,7 @@ public class DrawAxis {
 
                     }
                 }
+                //Algoritmus pre grafické osadenie mierky na os X aj s medzičiarami medzi číselne vyjadrenými bodmi mierky.
                 else {
                     graph.draw(new Line2D.Double(width/2- listOfScale[i]* axisScale, height/2-2,width/2- listOfScale[i]* axisScale, height/2+2));
                     graph.drawString(Double.toString(listOfScale[i]), Math.round((width / 2 + (listOfScale[i]* axisScale))-3),(int) height/2+15);
